2bdc441d90ef6a4e488f2e421d6ba181945d1d53
Michael Schleichardt <michael@scheichardt.net>
2012-11-01 18:22:23 +0100

    added heroku configuration

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,14 @@ object ApplicationBuild extends Build {
     val appVersion      = "0.1-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "info.schleichardt" %% "play-2-twitter-bootstrap-integration" % "0.1-SNAPSHOT"
+      , "info.schleichardt" %% "play-2-basic-auth" % "0.1-SNAPSHOT"
+      , "commons-io" % "commons-io" % "2.4"//heroku does not find it without the explicit dependency
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       coffeescriptOptions := Seq("bare")//coffee script code will not be wrapped in an anonymous function, necessary for tests
+      , resolvers += "schleichardts Github" at "http://schleichardt.github.com/jvmrepo/"
     )
 
 }
