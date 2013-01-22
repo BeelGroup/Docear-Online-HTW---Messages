4c3041a4cfc3c3fdad5e206ad584d339f2608f77
Paul Stüber <paul.stueber@web.de>
2013-01-22 23:15:15 +0100

    temporary increase of seconds before timeout in test

--- test/features/mindmap/node/VisualizationTest.java
+++ test/features/mindmap/node/VisualizationTest.java
@@ -24,8 +24,8 @@ public class VisualizationTest extends DocearHttpTest {
             @SuppressWarnings("unchecked") //type system is in fluentlenium library broken
             public void invoke(final TestBrowser testBrowser) throws Throwable {
                 testBrowser.goTo(url("/"));
-                testBrowser.executeScript(format("$('%s').click()", LOAD_MAP_3));//@hack
-                testBrowser.await().atMost(defaultWait(), SECONDS).until(new Predicate() {
+                testBrowser.executeScript(format("$('%s').click()", LOAD_MAP_3));//@hack defaultWait()
+                testBrowser.await().atMost(15, SECONDS).until(new Predicate() {
                     @Override
                     public boolean apply(@Nullable Object o) {
                         return testBrowser.pageSource().indexOf("What is Docear") > 0;
