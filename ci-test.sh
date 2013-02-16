c0c408699fd7bab72713b34e9dfa56002c4ac96d
Michael Schleichardt <michael@schleichardt.net>
2013-02-16 21:43:56 +0100

    more memory for ci tests

--- ci-test.sh
+++ ci-test.sh
@@ -1,4 +1,4 @@
 set -x
 
-export xsbt="java -Xms100M -Xmx200M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=128M  -jar sbtwrapper/sbt-launch.jar -Dsbt.log.noformat=true"
+export xsbt="java -Xms600M -Xmx600M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=600M  -jar sbtwrapper/sbt-launch.jar -Dsbt.log.noformat=true"
 $xsbt clean test
