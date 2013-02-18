588b4aeb0df4dad89e13664eec83b6b019c66ec2
Michael Schleichardt <michael@schleichardt.net>
2013-02-18 10:31:58 +0100

    updated to recent selenium and htmlunit versions, close #31

--- project/Build.scala
+++ project/Build.scala
@@ -7,26 +7,30 @@ object ApplicationBuild extends Build {
     val appName         = "Docear-Frontend"
     val appVersion      = "0.1-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "info.schleichardt" %% "play-2-basic-auth" % "0.3-SNAPSHOT"
-      , "commons-io" % "commons-io" % "2.4"//heroku does not find it without the explicit dependency
-      , "com.fasterxml.jackson.datatype" % "jackson-datatype-json-org" % "2.0.2"
-      , "commons-lang" % "commons-lang" % "2.6"
-      , "org.springframework" % "spring-context" % "3.1.2.RELEASE"
-      , "cglib" % "cglib" % "2.2.2"
-      , "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.25.0" % "test" //find new versions on http://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver
-      , "org.seleniumhq.selenium" % "selenium-chrome-driver" % "2.25.0" % "test" //find new versions on http://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver
-      , "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.25.0" % "test"
-      , "com.novocode" % "junit-interface" % "0.9" % "test"
-      , "org.reflections" % "reflections" % "0.9.8"//fix for error: NoSuchMethodError: com.google.common.cache.CacheBuilder.maximumSize(I)Lcom/google/common/cache/CacheBuilder;
-      , "joda-time" % "joda-time" % "2.1"
-      , jdbc
-      , javaCore
-      , javaJdbc
-      , javaEbean
-      , "org.webjars" % "webjars-play" % "2.1.0"
-      , "org.webjars" % "bootstrap" % "2.1.1"
-    )
+    val appDependencies = {
+      val seleniumVersion = "2.29.1"
+      Seq(
+        "info.schleichardt" %% "play-2-basic-auth" % "0.3-SNAPSHOT"
+        , "commons-io" % "commons-io" % "2.4"//heroku does not find it without the explicit dependency
+        , "com.fasterxml.jackson.datatype" % "jackson-datatype-json-org" % "2.0.2"
+        , "commons-lang" % "commons-lang" % "2.6"
+        , "org.springframework" % "spring-context" % "3.1.2.RELEASE"
+        , "cglib" % "cglib" % "2.2.2"
+        , "org.seleniumhq.selenium" % "selenium-firefox-driver" % seleniumVersion % "test" //find new versions on http://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver
+        , "org.seleniumhq.selenium" % "selenium-chrome-driver" % seleniumVersion % "test" //find new versions on http://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-firefox-driver
+        , "org.seleniumhq.selenium" % "selenium-htmlunit-driver" % seleniumVersion % "test"
+        , "org.apache.httpcomponents" % "httpclient" % "4.2.3" //needed by selenium, must be in compile scope
+        , "com.novocode" % "junit-interface" % "0.9" % "test"
+        , "org.reflections" % "reflections" % "0.9.8"//fix for error: NoSuchMethodError: com.google.common.cache.CacheBuilder.maximumSize(I)Lcom/google/common/cache/CacheBuilder;
+        , "joda-time" % "joda-time" % "2.1"
+        , jdbc
+        , javaCore
+        , javaJdbc
+        , javaEbean
+        , "org.webjars" % "webjars-play" % "2.1.0"
+        , "org.webjars" % "bootstrap" % "2.1.1"
+      )
+    }
 
     val handlebarsOptions = SettingKey[Seq[String]]("ember-options")
     val handlebarsEntryPoints = SettingKey[PathFinder]("ember-entry-points")
