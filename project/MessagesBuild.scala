52debd9d6f7ce55232aa1187d38555facd177740
Michael Schleichardt <michael@schleichardt.net>
2013-02-22 14:59:27 +0100

    enforce Java 6 for Debian Squeeze compatibility

--- project/MessagesBuild.scala
+++ project/MessagesBuild.scala
@@ -11,7 +11,8 @@ object MessagesBuild extends Build {
       organization := "org.docear",
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.10.0",
-      resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+      resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      javacOptions ++= Seq("-source", "1.6", "-target", "1.6")//for compatibility with Debian Squeeze
       // add other settings here
     ) ++ seq(com.github.retronym.SbtOneJar.oneJarSettings: _*)
   )
